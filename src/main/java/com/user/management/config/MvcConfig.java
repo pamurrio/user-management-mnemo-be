3b9b04f29465ae6cf901d37f1a639b7f2408dfe0
pdamurrio <71343147+pdamurrio@users.noreply.github.com>
2021-03-13 19:14:42 -0300

    correciones de bugs

--- src/main/java/com/user/management/config/MvcConfig.java
+++ src/main/java/com/user/management/config/MvcConfig.java
@@ -3,16 +3,17 @@ package com.user.management.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
-/*@Configuration
-@EnableJpaRepositories
-public class MyConfig implements WebMvcConfigurer {
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
 
     @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        argumentResolvers.add(new SpecificationArgumentResolver());
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
-}*/
+
+}
